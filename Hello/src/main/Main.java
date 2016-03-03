128335f085bdf9b708cc82d6276d13b11b80dbf3
VictorSoliman <vsoliman@outlook.com>
2016-03-03 18:03:07 +1100

    Update Main.java

--- Hello/src/main/Main.java
+++ Hello/src/main/Main.java
@@ -5,6 +5,11 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.print("helooooo !!");
+doThat(1,3);
 	}
+public void doThat(int a;int b){
+	int c = a + b;
+	System.out.print("The answer is " + c);
+}
 
 }
