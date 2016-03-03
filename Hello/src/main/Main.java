126df4ad939c100125a4b042c052f3d680ac5b78
vic <vic@192.168.1.2>
2016-03-03 18:38:13 +1100

    v3

--- Hello/src/main/Main.java
+++ Hello/src/main/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.print("helooooo !!");
+System.out.println("helooooo !!");
      doThat(1,3);
 	}
 	
